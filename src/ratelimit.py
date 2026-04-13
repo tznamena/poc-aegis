87dfb2f8a63511f824d13b70b867ab60122a9f5b
Tomas Z <tznamena@redhat.com>
2026-04-13 14:21:56 -0400

    Add rate limiting module

---
+++ src/ratelimit.py
@@ -0,0 +1,9 @@
+"""Rate limiting module."""
+
+MAX_REQUESTS_PER_MINUTE = 60
+
+
+def check_rate_limit(client_id):
+    """Check if a client has exceeded their rate limit."""
+    # placeholder
+    return True
